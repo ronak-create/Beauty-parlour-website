2afcfd05cfed21e70d27493c12ef8eaa4918e84c
ronak-create <84374383+ronak-create@users.noreply.github.com>
2024-05-28 00:08:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const info = document.getElementById("info-icon");
 const canclebutton = document.getElementById("cancle-button");
 abtbutton.addEventListener("click", () => {
   let aboutpage = document.getElementById("about-page");
-  aboutpage.style.left = "0";
+  setTimeout(() => {
+    aboutpage.style.display = "flex";
+  }, 10);
+  aboutpage.style.left = "0"; 
 });
 
 info.addEventListener("click", () => {
   let aboutpage = document.getElementById("about-page");
+  setTimeout(() => {
+    aboutpage.style.display = "flex";
+  }, 10);
   aboutpage.style.left = "0";
 });
-
 
 canclebutton.addEventListener("click", () => {
   let aboutpage = document.getElementById("about-page");
   aboutpage.style.left = "100%";
-})
+  setTimeout(() => {
+    aboutpage.style.display = "none";
+  }, 1000);
+});
